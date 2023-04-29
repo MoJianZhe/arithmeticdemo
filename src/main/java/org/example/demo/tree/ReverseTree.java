cac61839bbac769d57789e2eb78ddd4f09d3370d
linjun <86178583@qq.com>
2023-04-29 15:07:25 +0800

    feat(算法):翻转二叉树

---
+++ src/main/java/org/example/demo/tree/ReverseTree.java
@@ -0,0 +1,19 @@
+package org.example.demo.tree;
+
+/**
+ * @author lilinjun
+ * @date 2023/4/28
+ **/
+public class ReverseTree {
+
+    void reverseTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        reverseTree(root.left);
+        reverseTree(root.right);
+    }
+}
